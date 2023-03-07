207b260cefb356eafed8d3a59f18dd8dc08d1558
yangyachao <2405414352@qq.com>
2023-03-07 18:32:15 +0800

    +

--- sqd/rpc/reg/internal/logic/dologic.go
+++ sqd/rpc/reg/internal/logic/dologic.go
@@ -4,6 +4,7 @@ import (
 	"YxEmr/common"
 	"YxEmr/common/database"
 	"YxEmr/common/pub"
+	"YxEmr/sqd/rpc/per/perer"
 	"YxEmr/sqd/rpc/reg/internal/svc"
 	"YxEmr/sqd/rpc/reg/reg"
 	"context"
@@ -58,43 +59,36 @@ func (l *DoLogic) Do(in *reg.Req) (*reg.Resp, error) {
 	}
 	fsql += database.GetBranchInsertSql(regs, "TBREGSQDINFO")
 	//调用per.rpc执行
-
+	if _, err := l.svcCtx.Perer.Do(l.ctx, &perer.Req{
+		Ilx:   in.Ilx,
+		Ibrlx: in.Ibrlx,
+		Cbrh:  in.Cbrh,
+		Csqdh: in.Csqdh,
+		Cztbm: in.Cztbm,
+	}); err != nil {
+		return nil, err
+	}
+	//dtm分布式事务
 	//dtmServer := l.svcCtx.Config.Dtm
-	//req := &perer.Req{
-	//	Ilx:   in.Ilx,
-	//	Ibrlx: in.Ibrlx,
-	//	Cbrh:  in.Cbrh,
-	//	Csqdh: in.Csqdh,
-	//	Cztbm: in.Cztbm,
-	//}
 	//gid := shortuuid.New()
 	//msg := dtmgrpc.NewMsgGrpc(dtmServer, gid).
-	//	Add("localhost:9004/per.perer/Do", req)
-
-	//dx, _ := db.DB()
-	//err := msg.DoAndSubmitDB(fsql, dx, func(tx *sql.Tx) error {
-	//	_, err := tx.Exec(fsql)
-	//	fmt.Println("4", err)
-	//	return err
-	//})
-
+	//	Add("localhost:9004/per.perer/Do", in)
 	//tx := db.Begin()
 	//if err := tx.Exec(fsql).Error; err != nil {
 	//	tx.Rollback()
 	//	return nil, err
 	//}
-	//tx.Commit()
 	//if err := msg.Submit(); err != nil {
-	//	fmt.Println("err:", err)
+	//	fmt.Println("per err:", err)
 	//	tx.Rollback()
-	//	return nil, err
 	//}
-	//手动代码结束
+	//
+	//tx.Commit()
+
+	//执行登记sql
 	if err := database.Exesql(fsql); err != nil {
 		return nil, err
 	}
 
 	return nil, nil
-
-	//return nil
 }
